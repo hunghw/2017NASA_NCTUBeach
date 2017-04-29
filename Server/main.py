bf9aaa7234fa3c34e9c5bc4ee69742ee9d2f7400
Howard Hung <hwhung0111@gmail.com>
2017-04-30 03:21:03 +0800

    Update

--- Server/main.py
+++ Server/main.py
@@ -3,6 +3,9 @@ import BaseHTTPServer
 
 from urlparse import urlparse
 import json
+import urllib2 
+import urllib
+from getColor import getColor
 
 HOST_NAME = '140.113.235.154' # !!!REMEMBER TO CHANGE THIS!!!
 PORT_NUMBER = 9000 # Maybe set this to 9000.
@@ -17,15 +20,35 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         query = urlparse(s.path)[4]
         if query != '':
             input_data = {}
+            output_data = {}
             for list_query in query.split('&'):
                 tmp = list_query.split('=')
                 input_data[tmp[0]] = tmp[1]
-            # time latitude longitude
+            # lat long
+
+            url_obs = 'https://bde75dae-f931-4098-8c54-1113ac61bc24:pTdEQXXoOS@twcservice.mybluemix.net:443/api/weather/v1/geocode/' + input_data['lat'] + '/' + input_data['long'] + '/observations.json?units=m&language=en-US'
+            url_for = 'https://bde75dae-f931-4098-8c54-1113ac61bc24:pTdEQXXoOS@twcservice.mybluemix.net:443/api/weather/v1/geocode/' + input_data['lat'] + '/' + input_data['long'] + '/forecast/hourly/48hour.json?units=m&language=en-US'
+            print url_obs
+            print url_for
+            response = urllib.urlopen(url_obs)
+            data = json.loads(response.read())
+            output_data['uv_desc'] = data['observation']['uv_desc']
+            output_data['temp'] = data['observation']['temp']
+            output_data['wx_phrase'] = data['observation']['wx_phrase']
+            response = urllib.urlopen(url_for)
+            data = json.loads(response.read())
+            output_data['phrase_32char'] = data['forecasts'][0]['phrase_32char']
+
+            output_data['Chlorophyll'] = getColor(float(input_data['lat']),float(input_data['long']))
+
+            print '=============='
+            print output_data
+            print '=============='
             """Respond to a GET request."""
             s.send_response(200)
             s.send_header("Content-type", "application/json")
             s.end_headers()
-            s.wfile.write(json.dumps(input_data, file, indent=4))
+            s.wfile.write(json.dumps(output_data, file, indent=4))
         else: 
             """Respond to a GET request."""
             s.send_response(200)
